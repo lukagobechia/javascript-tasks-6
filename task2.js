8a3add53280f8585e2586f76db9feefe0b155035
Luka Gobechia <lukagobechiaa@gmail.com>
2024-10-06 19:21:17 +0400

    added task 2

---
+++ task2.js
@@ -0,0 +1,11 @@
+const url = "https://jsonplaceholder.typicode.com/users";
+function fetchData(url) {
+  return new Promise((res, rej) => {
+    fetch(url)
+      .then(data => res(data))
+      .catch(e => rej(e));
+  });
+}
+fetchData(url)
+  .then(data => console.log(data))
+  .catch(e => console.log("error: ", e));
