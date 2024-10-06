8a1b75f17f44df521a396f689333078944858602
Luka Gobechia <lukagobechiaa@gmail.com>
2024-10-06 19:49:49 +0400

    added task 3

---
+++ task3.js
@@ -0,0 +1,25 @@
+const url = "https://jsonplaceholder.typicode.com";
+function sleep(ms) {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res();
+    }, ms);
+  });
+}
+async function fetchData(url) {
+  for (let i = 0; i < 5; i++) {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      return;
+    } catch (e) {
+      console.log("error retrieving data");
+      await sleep(2000);
+      if (i == 4)
+        console.log("we tried retrieving data 5 times. Could not retrieved Users data");
+    }
+  }
+}
+
+fetchData(url);
