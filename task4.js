71649f86110cdafbffd496e2acf861c9651bf005
Luka Gobechia <lukagobechiaa@gmail.com>
2024-10-06 21:13:17 +0400

    added task 4

--- task4.js
+++ task4.js
@@ -1,17 +1,17 @@
 const url1 = "https://dummyjson.com/users";
 const url2 = "https://jsonplaceholder.typicode.com/users";
-const p1 = new Promise((res, rej) => {
-  fetch(url1)
-    .then((resp) => resp.json())
-    .then((data) => res(data))
-    .catch((e) => rej(e));
-});
-const p2 = new Promise((res, rej) => {
-  fetch(url2)
-    .then((resp) => resp.json())
-    .then((data) => res(data))
-    .catch((e) => rej(e));
-});
+
+function fetchData(url) {
+  return new Promise((res, rej) => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => res(data))
+      .catch((e) => rej(e));
+  });
+}
+
+const p1 = fetchData(url1);
+const p2 = fetchData(url2);
 
 Promise.any([p1, p2])
   .then((res) => console.log(res))
