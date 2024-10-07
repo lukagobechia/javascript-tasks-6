3701cfe9565d42e6f13079876a2babecf95acd56
Luka Gobechia <lukagobechiaa@gmail.com>
2024-10-07 21:45:54 +0400

    added task 3

--- task3.js
+++ task3.js
@@ -6,20 +6,25 @@ function sleep(ms) {
     }, ms);
   });
 }
+let retryCount = 0;
+const maxRetrycount = 5;
 async function fetchData(url) {
-  for (let i = 0; i < 5; i++) {
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data);
-      return;
-    } catch (e) {
-      console.log("error retrieving data");
-      await sleep(2000);
-      if (i == 4){
-          console.log("\nwe tried retrieving data 5 times. Could not retrieved Users data\n");
-          console.log('Error message:\n', e.message)
-      }
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    return;
+  } catch (e) {
+    await sleep(2000);
+    if (retryCount < maxRetrycount) {
+      retryCount++;
+      console.log("error retrieving data", retryCount);
+      fetchData(url);
+    } else {
+      console.log(
+        "\nwe tried retrieving data 5 times. Could not retrieved Users data\n"
+      );
+      console.log("Error message:\n", e.message);
     }
   }
 }
